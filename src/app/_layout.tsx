84312b065bfa0ec8ee54ccc4845091f5ec16b7dd
Exmarshall <umarhayatudeen88@gmail.com>
2025-10-08 13:58:19 +0100

    initial video cal setup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,8 +3,26 @@ import { Slot } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import AuthProvider from "../providers/AuthProvider";
+import { PermissionsAndroid, Platform } from "react-native";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  useEffect(() => {
+    const run = async () => {
+      if (Platform.OS === "android") {
+        try {
+          await PermissionsAndroid.requestMultiple([
+            PermissionsAndroid.PERMISSIONS.POST_NOTIFICATIONS,
+            PermissionsAndroid.PERMISSIONS.BLUETOOTH_CONNECT,
+          ]);
+        } catch (err) {
+          console.warn("Permission request error:", err);
+        }
+      }
+    };
+
+    run();
+  }, []);
   return (
 
     <GestureHandlerRootView style={{ flex: 1 }}>
